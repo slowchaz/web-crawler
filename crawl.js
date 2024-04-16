34bff964da85fa1c4f86ebe6e380a528aa3047ee
chaz <corfitzgerald@gmail.com>
2024-04-16 15:43:48 -0500

    starting crawl:

--- crawl.js
+++ crawl.js
@@ -1,16 +1,66 @@
-normalizeURL = function (url) {
-  const urlObj = new URL(url)
-  let hostname = urlObj.hostname
-  let pathname = urlObj.pathname
+const { JSDOM } = require('jsdom')
+
+
+async function crawlPage(url) {
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch page: ${response.status}`)
+    } else if (!response.headers.get('content-type').includes('text/html')) {
+      throw new Error('Page is not HTML')
+    } else {
+      return await response.text()
+    }
+
+  } catch (err) {
+    console.log(err.message)
+    return null
+  }
+
+}
+
+
+function getURLsFromHTML(html, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(html);
+  const links = dom.window.document.querySelectorAll('a');
+
+  links.forEach(link => {
+    if (link.href.slice(0, 1) === '/') {
+      try {
+        urls.push(new URL(link.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${link.href}`);
+      }
+    } else {
+      try {
+        urls.push(new URL(link.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${link.href}`);
+      }
+    }
+  });
+
+  return urls;
+}
+
+
+function normalizeURL(url) {
+  const urlObj = new URL(url);
+  let hostname = urlObj.hostname;
+  let pathname = urlObj.pathname;
 
   if (pathname.endsWith('/')) {
     pathname = pathname.slice(0, -1);
   }
 
-  return `${hostname}${pathname}`
+  return `${hostname}${pathname}`;
 }
 
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML,
+  crawlPage
 }
--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const { normalizeURL } = require('./crawl.js')
+const { crawlPage } = require('./crawl.js')
+
+// accept command line arguments
+async function main() {
+  const args = process.argv.slice(2)
+
+  if (args.length === 0) {
+    console.log('Please provide a URL')
+    return
+  }
+  if (args.length > 1) {
+    console.log('Please provide only one URL')
+    return
+  }
+
+  const baseURL = args[0]
+  console.log(`Crawling ${baseURL}`)
+  await crawlPage(baseURL)
+}
 
 
-console.log(normalizeURL('https://blog.boot.dev/path/'))
+main()
